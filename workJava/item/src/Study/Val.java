12f6e8e78b63a03ec3b436a4b9256fa344584e42
Aimee <AimeeAngelia2022@outlook.com>
2022-07-27 16:09:05 +0800

    Delete workJava/item directory

--- workJava/item/src/Study/Val.java
+++
@@ -1,9 +0,0 @@
-package Study;
-
-public class Val {
-    static int times = 3;
-    public static void main(String[] args) {
-        int times = 4;
-        System.out.println("times的值是" + times + "，times的值是" + Val.times);
-    }
-}
